4d4c82bc3a0c35d725842ca59fcdae13bae59483
Frank-Bedoya <franksurenuga@gmail.com>
2023-03-09 20:07:10 -0500

    Bases del proyecto y inicio del header

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+export const Header = () => {
+return(
+    <div className="header">
+    <div className="header__izquierda">
+        <img src="img/Romeo.png" alt="logo" />
+    </div>
+    <div>
+        <ul>
+            <li>Inicio</li>
+            <li>Sobre nosotros</li>
+        </ul>
+        </div>
+    
+    </div>
+    
+)
+}
