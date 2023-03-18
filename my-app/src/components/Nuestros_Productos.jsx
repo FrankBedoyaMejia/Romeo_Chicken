91b1eb4b2746ec21a4aa1c3a210a356a198bf7b6
Frank-Bedoya <franksurenuga@gmail.com>
2023-03-17 23:25:56 -0500

    Realizando organizacion de estilos de header y tarjetas de productos

--- my-app/src/components/Nuestros_Productos.jsx
+++ my-app/src/components/Nuestros_Productos.jsx
@@ -1,29 +1,19 @@
 import React from "react";
 import productos from "../Data/Productos.json";
-import {
-  Card_Producto,
-  Card_Producto_preciosolo,
-  Card_Producto_filetes,
-} from "./Card_Producto";
+import { Card_Producto } from "./Card_Producto";
 
 export const Nuestros_Productos = () => {
   return (
     <>
       <h1 className="section">Productos mas Vendidos</h1>
       <div className="section-productos">
-        <Card_Producto key={productos[1].titulo} producto={productos[1]} />
-
-        <Card_Producto_filetes
-          key={productos[0].titulo}
-          producto={productos[0]}
-        />
-        <Card_Producto_preciosolo
-          key={productos[2].titulo}
-          producto={productos[2]}
-        />
-
-        {/* <div className="section__izquierda"></div>
-      <div className="section__derecha"></div> */}
+        {productos
+          ? productos.map((producto) => {
+              return (
+                <Card_Producto key={producto.titulo} producto={producto} />
+              );
+            })
+          : null}
       </div>
     </>
   );
