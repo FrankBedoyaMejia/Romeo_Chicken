a384b1863779c445454a5e36087d1105b5a090f9
FrankBedoyac4 <franksurenuga@gmail.com>
2023-03-18 13:13:52 -0500

    Organizacion de las estilos para que la pagina se vea mas uniforme en colores y organizacion de section de videos

--- my-app/src/components/Nuestras_Recetas.jsx
+++ my-app/src/components/Nuestras_Recetas.jsx
@@ -4,35 +4,41 @@ export const Nuestras_Recetas = () => {
   return (
     <>
       <div>
-        <h1 className="section">Algunas Recetas con nuestros productos</h1>
-
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/8fz_GMOP5Qc"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/VRf8GGR9Chs"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/fhv8cYQkLII"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
+        <h1 className="section_recetas">
+          Algunas Recetas con nuestros productos
+        </h1>
+        <div className="section_recetas-contenedor">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/8fz_GMOP5Qc"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            className="section_recetas-videos"
+          ></iframe>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/VRf8GGR9Chs"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            className="section_recetas-videos"
+          ></iframe>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/fhv8cYQkLII"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            className="section_recetas-videos"
+          ></iframe>
+        </div>
       </div>
     </>
   );
--- my-app/src/components/Nuestros_Productos.jsx
+++ my-app/src/components/Nuestros_Productos.jsx
@@ -5,8 +5,8 @@ import { Card_Producto } from "./Card_Producto";
 export const Nuestros_Productos = () => {
   return (
     <>
-      <h1 className="section">Productos mas Vendidos</h1>
-      <div className="section-productos">
+      <h1 className="section_prductos">Productos mas Vendidos</h1>
+      <div className="section_prductos-productos">
         {productos
           ? productos.map((producto) => {
               return (
