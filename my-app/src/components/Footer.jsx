a5cb488996c397e972d2d5528cdffa1565a56199
Frank-Bedoya <franksurenuga@gmail.com>
2023-03-27 18:30:55 -0500

    Adelanto de footer y agregando libreria de fontawesome

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const Footer = () => {
   return (
     <>
-      <footer className="footer">Poque ya no salgo</footer>
+      <footer className="footer">
+        <div className="footer-izquierda">
+          {/* <img src="/img/Romeo1.png" alt="Logo Romeo chicken" /> */}
+          <span>Romeo Chicken</span>
+        </div>
+        <FontAwesomeIcon
+          icon="fa-brands fa-whatsapp"
+          style={{ color: "#ffffff" }}
+        />
+      </footer>
     </>
   );
 };
