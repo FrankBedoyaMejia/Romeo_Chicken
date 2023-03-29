18ef6627baa72ea4f197732ecd9b803f18d25160
FrankBedoyac4 <franksurenuga@gmail.com>
2023-03-28 23:51:14 -0500

    se añadio icono de whatsapp y se empezo la creacion de contexto global y carrito de compra

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {} from "@fortawesome/free-solid-svg-icons";
+// import {} from "@fortawesome/free-solid-svg-icons";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 export const Footer = () => {
   return (
@@ -10,7 +10,15 @@ export const Footer = () => {
           {/* <img src="/img/Romeo1.png" alt="Logo Romeo chicken" /> */}
           <span>Romeo Chicken</span>
         </div>
-        <FontAwesomeIcon icon={faWhatsapp} />
+        <div className="footer-derecha">
+          <a href="https://example.com" target="_blank">
+            <FontAwesomeIcon
+              icon={faWhatsapp}
+              className="footer-icono"
+              size="2xl"
+            />
+          </a>
+        </div>
       </footer>
     </>
   );
