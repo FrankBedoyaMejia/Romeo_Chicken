c04a248a2acdca99e19ce5a9babbe63c58617f75
FrankBedoyaMejia <franksurenuga@gmail.com>
2023-04-13 13:01:09 -0500

    Realizando de nuevo los modal por perdida de datos entre pc´s

--- my-app/src/components/Modal.jsx
+++ my-app/src/components/Modal.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 
-export const ModalFilete = ({ open }) => {
+export const ModalFilete = ({ open, setOpenFiletes }) => {
+  const onClick = () => {
+    setOpenFiletes(false);
+  };
   if (!open) return null;
   return (
     <div className="overlay">
       <div className="overlay__background">
         <h1>Hola Filete</h1>
+        <button className="overlay__button" onClick={() => onClick()}>
+          Cerrar
+        </button>
       </div>
     </div>
   );
 };
 
-export const ModalChuzo = ({ open }) => {
+export const ModalChuzo = ({ open, setOpenChuzo }) => {
+  const onClick = () => {
+    setOpenChuzo(false);
+  };
   if (!open) return null;
 
   return (
     <div className="overlay">
       <div className="overlay__background">
         <h1>Hola Chuzo</h1>
+        <button className="overlay__button" onClick={() => onClick()}>
+          Cerrar
+        </button>
       </div>
     </div>
   );
